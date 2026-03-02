d2188ee4f9670ded14833d3b628a3226332e08b5
Alicia2202 <alicialf02@gmail.com>
2026-03-02 20:46:44 +0000

    Feat: Creado el generador de cartas básico

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard();
+};
+
+const generateRandomCard= () => {
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  const suits = ["diamond", "heart", "spade", "club"];
+  const randomValues = getRandomElement(values);
+  const randomsuits = getRandomElement(suits);
+  renderCard(randomValues, randomsuits)
+}
+
+const getRandomElement = (array) => array[Math.floor(Math.random()*array.length)];
+
+const renderCard= (value, suit) => {
+  const icons = {
+    diamond:"♦", 
+    heart: "♥", 
+    spade: "♠", 
+    club: "♣"
+  };
+  const card = document.querySelector("#myCard");
+  const valueCard = document.querySelector("#value-card");
+  const topSuit = document.querySelector("#top-suit");
+  const bottomSuit = document.querySelector("#bottom-suit");
+  valueCard.innerHTML = value;
+  topSuit.innerHTML = icons[suit];
+  bottomSuit.innerHTML = icons[suit];
+  card.className = `card poker-card shadow-lg position-relative ${suit}`;
 };
