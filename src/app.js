217d05c374924a60fb878523e0a426f96bccdb60
Alicia2202 <alicialf02@gmail.com>
2026-03-03 19:19:25 +0000

    Refactor: Cambio de objeto de lo iconos  a un array en js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,10 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function () {
-  //write your code here
   generateRandomCard();
 };
 
 const generateRandomCard = () => {
   const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
-  const suits = ["diamond", "heart", "spade", "club"];
+  const suits = ["♦", "♥", "♠", "♣"];
   const randomValues = getRandomElement(values);
   const randomsuits = getRandomElement(suits);
   renderCard(randomValues, randomsuits)
@@ -21,22 +13,16 @@ const generateRandomCard = () => {
 const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
 
 const renderCard = (value, suit) => {
-  const icons = {
-    diamond: "♦",
-    heart: "♥",
-    spade: "♠",
-    club: "♣"
-  };
   const card = document.querySelector("#myCard");
   const valueCard = document.querySelector("#value-card");
   const topSuit = document.querySelector("#top-suit");
   const bottomSuit = document.querySelector("#bottom-suit");
 
   valueCard.innerHTML = value;
-  topSuit.innerHTML = icons[suit];
-  bottomSuit.innerHTML = icons[suit];
+  topSuit.innerHTML = suit;
+  bottomSuit.innerHTML = suit;
 
-  if (suit === "heart" || suit === "diamond") {
+  if (suit === "♥" || suit === "♦") {
     valueCard.style.color = "red";
     topSuit.style.color = "red";
     bottomSuit.style.color = "red";
@@ -45,5 +31,5 @@ const renderCard = (value, suit) => {
     topSuit.style.color = "black";
     bottomSuit.style.color = "black";
   }
-  // card.className = `card poker-card shadow-lg position-relative ${suit}`;
+
 };
