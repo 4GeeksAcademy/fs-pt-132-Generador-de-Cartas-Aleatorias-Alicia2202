82199764adc342c3dad73ceeb14191d87d55f884
Alicia2202 <alicialf02@gmail.com>
2026-03-03 15:28:58 +0000

    Chore: Añadido color de los palos y número en js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   generateRandomCard();
 };
 
-const generateRandomCard= () => {
+const generateRandomCard = () => {
   const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
   const suits = ["diamond", "heart", "spade", "club"];
   const randomValues = getRandomElement(values);
@@ -18,21 +18,32 @@ const generateRandomCard= () => {
   renderCard(randomValues, randomsuits)
 }
 
-const getRandomElement = (array) => array[Math.floor(Math.random()*array.length)];
+const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
 
-const renderCard= (value, suit) => {
+const renderCard = (value, suit) => {
   const icons = {
-    diamond:"♦", 
-    heart: "♥", 
-    spade: "♠", 
+    diamond: "♦",
+    heart: "♥",
+    spade: "♠",
     club: "♣"
   };
   const card = document.querySelector("#myCard");
   const valueCard = document.querySelector("#value-card");
   const topSuit = document.querySelector("#top-suit");
   const bottomSuit = document.querySelector("#bottom-suit");
+
   valueCard.innerHTML = value;
   topSuit.innerHTML = icons[suit];
   bottomSuit.innerHTML = icons[suit];
-  card.className = `card poker-card shadow-lg position-relative ${suit}`;
+
+  if (suit === "heart" || suit === "diamond") {
+    valueCard.style.color = "red";
+    topSuit.style.color = "red";
+    bottomSuit.style.color = "red";
+  } else {
+    valueCard.style.color = "black";
+    topSuit.style.color = "black";
+    bottomSuit.style.color = "black";
+  }
+  // card.className = `card poker-card shadow-lg position-relative ${suit}`;
 };
